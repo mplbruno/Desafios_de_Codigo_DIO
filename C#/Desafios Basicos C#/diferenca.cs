f356f793266274e20a862f80455bf0c9a5c6d739
mplbruno <mplbruno@gmail.com>
2021-10-10 16:14:57 -0300

    Atualização da Pasta de Desafios

---
+++ C#/Desafios Basicos C#/diferenca.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Desafio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          int A, B, C, D;
+      
+      A = int.Parse(Console.ReadLine());
+      B = int.Parse(Console.ReadLine());
+      C = int.Parse(Console.ReadLine());
+      D = int.Parse(Console.ReadLine());
+      
+      Console.WriteLine("DIFERENCA = " + (A * B - C * D));
+      Console.ReadKey();
+      
+        }
+    }
+}
---
+++ C#/Desafios Basicos C#/felizNataaal.cs
@@ -0,0 +1,18 @@
+using System; 
+
+class TESTE {
+
+    static void Main(string[] args) { 
+
+       int x = int.Parse(Console.ReadLine());
+            Console.Write("Feliz nat");
+            for (int contador = 0;  contador < x ;contador ++ )
+            {
+                Console.Write("a");
+            }
+
+            Console.Write("l!\n");
+
+    }
+
+}
